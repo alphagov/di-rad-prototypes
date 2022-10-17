05967cd8340770e3da11acaf9d366891435cb1bd
Sonia Turcotte <sonia.turcotte@gmail.com>
2022-10-17 12:14:57 +0100

    remove hilarious typo from service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'GDX Vision'
+  serviceName: 'GDX Vision',
 
   // Default port that prototype runs on
   port: '3000',
